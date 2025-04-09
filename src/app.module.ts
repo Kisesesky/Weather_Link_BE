be15c260d6f7e1a2be1a3224a111e54661d0f4d5
Hoya <kisesesky19@gmail.com>
2025-04-09 21:54:06 +0900

    feat: chatmodule 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,10 @@ import { AuthModule } from './modules/auth/auth.module';
 import { LocationsModule } from './modules/locations/locations.module';
 import { WeatherModule } from './modules/weather/weather.module';
 import { FriendsModule } from './modules/friends/friends.module';
+import { ChatModule } from './modules/chat/chat.module';
 
 @Module({
-  imports: [UsersModule, AuthModule, LocationsModule, WeatherModule, FriendsModule],
+  imports: [UsersModule, AuthModule, LocationsModule, WeatherModule, FriendsModule, ChatModule],
   controllers: [AppController],
   providers: [AppService],
 })
