31ac9ed1b788a149c3abc1ae88a97262aacbafb6
Hoya <kisesesky19@gmail.com>
2025-04-23 22:04:02 +0900

    fix:s3.service

--- src/modules/s3/s3.service.ts
+++ src/modules/s3/s3.service.ts
@@ -24,6 +24,7 @@ export class S3Service {
       Key: fileName,
       Body: file.buffer,
       ContentType: file.mimetype,
+      ASL: 'public-read',
     };
 
     await this.s3.send(new PutObjectCommand(uploadParams));
