06821630cab6c33254af76834befb9fb32531f8a
Hoya <kisesesky19@gmail.com>
2025-04-30 11:59:27 +0900

    fix:소셜로그인 부분 수정

--- src/modules/auth/guards/google-auth.guard.ts
+++ src/modules/auth/guards/google-auth.guard.ts
@@ -1,15 +1,52 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Inject,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { REQUEST } from '@nestjs/core';
 import { Request } from 'express';
 
 @Injectable()
 export class GoogleAuthGuard extends AuthGuard('google') {
-  handleRequest(err, user, info) {
-    // err가 있거나 user가 false인데 info도 없는 경우 (예상치 못한 오류) -> 기본 오류 처리
+  constructor(@Inject(REQUEST) private request: Request) {
+    super();
+  }
+
+  getAuthenticateOptions(context: ExecutionContext): any {
+    const request = context.switchToHttp().getRequest<Request>();
+    let origin = request.query.origin as string;
+
+    const allowedOrigins = [
+      'http://localhost:3000', // 개발용 프론트 경로
+      'https://www.weather-link.site', // 배포용 프론트 경로
+      // 필요한 경우 다른 경로 추가
+    ];
+    if (!origin || !allowedOrigins.includes(origin)) {
+      console.warn(
+        `Invalid or missing origin for OAuth redirect: ${origin}. Using default.`,
+      );
+
+      origin = 'https://www.weather-link.site';
+    }
+
+    const stateObject = {
+      origin: origin,
+      nonce: Math.random().toString(36).substring(7),
+    };
+    const encodedState = Buffer.from(JSON.stringify(stateObject)).toString(
+      'base64',
+    );
+
+    return { state: encodedState };
+  }
+
+  handleRequest(err, user, info, context: ExecutionContext) {
     if (err || (!user && !info)) {
       throw err || new UnauthorizedException();
     }
+
     // user가 false이지만 info가 있는 경우 (신규 사용자) -> user 대신 info.profile을 반환
     if (!user && info?.profile) {
       // info와 info.profile 존재 여부 확인
